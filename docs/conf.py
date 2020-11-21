165f42463c353e6c40a625e49cad21147731ec2a
Jonathan Yong <yongrenjie@gmail.com>
2020-11-21 02:41:40 +0000

    minor cleanup of config files

--- docs/conf.py
+++ docs/conf.py
@@ -72,7 +72,7 @@ intersphinx_mapping = {
 default_role = "any"
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+# templates_path = ['_templates']
 
 # Global reST substitutions
 rst_prolog = """
@@ -117,4 +117,4 @@ html_theme = 'alabaster'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']

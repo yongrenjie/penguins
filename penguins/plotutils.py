7114db0037cf9c69f601d165b6406b3dc4dd9f32
Jonathan Yong <yongrenjie@gmail.com>
2020-11-24 16:25:10 +0000

    Add label_axes() utility function (#12)

--- penguins/plotutils.py
+++ penguins/plotutils.py
@@ -7,8 +7,10 @@ wide range of figures.
 """
 
 import warnings
-from typing import (Any)
+from typing import (Any, Sequence, Tuple)
 
+import numpy as np  # type: ignore
+import matplotlib
 import matplotlib.pyplot as plt  # type: ignore
 from matplotlib.ticker import AutoMinorLocator  # type: ignore
 
@@ -175,3 +177,104 @@ def move_ylabel(ax: Any,
 
     if tight_layout:
         plt.tight_layout()
+
+
+def label_generator(form):
+    """
+    Internal generator which spits out abcdefghijkl... or 1234567.... or worse,
+    Roman numerals.
+    """
+    chars = "abcdefghijklmnopqrstuvwxyz"
+    roman = ("i ii iii iv v vi vii viii ix x xi xii xiii xiv xv xvi"
+             " xvii xviii xix xx xxi xxii xxiii xxiv xxv xxvi")
+    if form == "a":
+        while True:
+            yield from chars
+    elif form == "A":
+        while True:
+            yield from chars.upper()
+    elif form == "i":
+        while True:
+            yield from roman.split()
+    elif form == "I":
+        while True:
+            yield from roman.upper().split()
+    elif form == "1":
+        count = 1
+        while True:
+            yield count
+            count = count + 1
+
+
+@export
+def label_axes(axs: Any,
+               pos: str = "upper left",
+               offset: Tuple[float, float] = (0.02, 0.02),
+               form: str = "a",
+               start: int = 1,
+               fstr: str = "{}",
+               **kwargs: Any
+               ) -> None:
+    """
+    Adds consecutive labels to the corners of a series of |Axes|. Useful for
+    creating numbered plots.
+
+    Parameters
+    ----------
+    axs : sequence of |Axes|
+    pos : str, default {"upper left"}
+        The location to place the text at. Right now the only option is the
+        upper left corner.
+    offset : (float, float), default (0.02, 0.02)
+        The horizontal and vertical offsets from the edges of the Axes, in Axes
+        coordinates. In other words, the default offset places 2% of space
+        between the edge of the Axes and the text.
+    form : str from {"a", "A", "i", "I", "1"}, default "a"
+        The numbers, or characters, to use for numbering. The alphabet work up
+        to 26 times (i.e. 'z'). Roman numerals work up to 26 (i.e. 'xxvi'). If
+        you need more than that, you're probably doing your plots wrongly.
+    start : int, default 1
+        The number to start from. Values below 1 are ignored.
+    fstr : str, default "{}"
+        A format string with a single field. The actual string placed on the
+        axes will be fstr.format(char), where char is the actual counter. Thus,
+        if you want your graphs to be labelled (a), (b), ... set fstr to be
+        "({})", for example.
+    **kwargs : dict
+        Optional parameters which are passed to ax.text(). If there is a clash,
+        the parameters passed here will override choices made using the other
+        keyword arguments. For example, if you want your text in the middle of
+        the spectrum, you can call `label_axes(..., x=0.5, y=0.5)`.
+    """
+    # Convert whatever sequence we throw at it
+    if isinstance(axs, matplotlib.axes.Axes):
+        axs = [axs]
+    axs_flat = np.array(axs).flat
+    # Figure out text placement. We need to use kwargs.pop() to make sure that
+    # we don't pass the same keyword argument twice to ax.text() later on.
+    if pos == "upper left":
+        x = kwargs.pop("x", 0 + offset[0])
+        y = kwargs.pop("y", 1 - offset[1])
+        ha = kwargs.pop("horizontalalignment", "left")
+        va = kwargs.pop("verticalalignment", "top")
+    else:
+        raise ValueError(f"invalid value '{pos}' provided for pos")
+    # Figure out other parameters. This is a bit odd, but we need to do this to
+    # (1) avoid passing the same argument twice to ax.text(), as before, but
+    # also (2) avoid popping the value on the first Axes and then not having it
+    # for subsequent ones. This is not an issue for x, y, ha, and va because
+    # those are constants for all Axes, whereas s and transform are in general
+    # not.
+    kwargs_s = kwargs.pop("s", None)
+    kwargs_trfm = kwargs.pop("trfm", None)
+    # Prime the character generator
+    gen = label_generator(form)
+    while start > 1:
+        next(gen)
+        start = start - 1
+    # Do the plotting
+    for ax in axs_flat:
+        s = kwargs_s or fstr.format(next(gen))
+        trfm = kwargs_trfm or ax.transAxes
+        ax.text(x=x, y=y, s=s, transform=trfm,
+                horizontalalignment=ha, verticalalignment=va, **kwargs)

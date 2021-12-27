0d7f12e9e4d03c6f3ee47b9dbc602b9dc80a4639
Jonathan Yong <yongrenjie@gmail.com>
2021-12-27 19:35:15 +0000

    expose palettes as easy-to-access constants

--- penguins/__init__.py
+++ penguins/__init__.py
@@ -2,3 +2,12 @@ from .dataset import *
 from .pgplot import *
 from .plotutils import *
 from ._version import __version__
+
+
+# Easy access to color palettes
+DEEP = color_palette('deep')
+MUTED = color_palette('muted')
+PASTEL = color_palette('pastel')
+BRIGHT = color_palette('bright')
+DARK = color_palette('dark')
+COLORBLIND = color_palette('colorblind')
